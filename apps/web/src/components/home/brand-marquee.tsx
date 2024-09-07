7c1403e12000592ea662fec171152beb1be9425a
SankhaChak <anonproductionspro@gmail.com>
2024-09-07 08:46:45 +0530

    chore: ⚓ update brand marquee

--- apps/web/src/components/home/brand-marquee.tsx
+++ apps/web/src/components/home/brand-marquee.tsx
@@ -1,19 +1,29 @@
-import OptimismIcon from "@yieldhive/ui/components/icons/optimism";
+import ModeIcon from "@yieldhive/ui/components/icons/mode";
+import PythIcon from "@yieldhive/ui/components/icons/pyth";
+import SuperformIcon from "@yieldhive/ui/components/icons/superform";
+import TenderlyIcon from "@yieldhive/ui/components/icons/tenderly";
 import Marquee from "react-fast-marquee";
 
 const BrandMarquee = () => {
+  const icons = [
+    <TenderlyIcon className="w-48" />,
+    <ModeIcon className="w-28" />,
+    <SuperformIcon className="w-8 h-8 mx-4" />,
+    <PythIcon className="w-28" />,
+  ];
+
   return (
     <div className="pb-10">
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden container mx-auto">
         <Marquee pauseOnHover loop={0} speed={100}>
-          {Array(10)
-            .fill(0)
-            .map((_, i) => (
-              <div key={i} className="px-10 filter grayscale opacity-60">
-                <OptimismIcon />
-                {/* Should be replaced with all other icons */}
-              </div>
-            ))}
+          {icons.map((icon, index) => (
+            <div
+              key={index}
+              className="px-4 filter grayscale opacity-60 hover:grayscale-0 hover:opacity-100 transition-all duration-300"
+            >
+              {icon}
+            </div>
+          ))}
         </Marquee>
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background z-10"></div>
         <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background z-10"></div>
